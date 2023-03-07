ce6aebcd6d160662ad8d622bfedf93f8d0271a55
damiendesvent <damiendesvent@gmail.com>
2023-03-07 15:33:34 -0500

    formattage et mise au propre du code

--- src/main.py
+++ src/main.py
@@ -1,29 +1,29 @@
-from signalrcore.hub_connection_builder import HubConnectionBuilder
 import logging
 import sys
-import requests
 import json
 import time
 import os
-import mysql.connector as mysql
+import requests
+from signalrcore.hub_connection_builder import HubConnectionBuilder
+
 
 class Main:
     def __init__(self):
         self._hub_connection = None
-        self.HOST = os.environ["HVAC_HOST"]
-        self.TOKEN = os.environ["HVAC_TOKEN"]
-    
+        self.host = os.environ["HVAC_HOST"]
+        self.token = os.environ["HVAC_TOKEN"]
+
     def __del__(self):
-        if (self._hub_connection != None):
+        if self._hub_connection is not None:
             self._hub_connection.stop()
 
     def setup(self):
-        self.setSensorHub()        
+        self.set_sensor_hub()
 
     def start(self):
         self.setup()
         self._hub_connection.start()
-        
+
         print("Press CTRL+C to exit.")
         while True:
             time.sleep(2)
@@ -31,46 +31,57 @@ class Main:
         self._hub_connection.stop()
         sys.exit(0)
 
-    def setSensorHub(self):
-        self._hub_connection = HubConnectionBuilder()\
-        .with_url(f"{self.HOST}/SensorHub?token={self.TOKEN}")\
-        .configure_logging(logging.INFO)\
-        .with_automatic_reconnect({
-            "type": "raw",
-            "keep_alive_interval": 10,
-            "reconnect_interval": 5,
-            "max_attempts": 999
-        }).build()
+    def set_sensor_hub(self):
+        self._hub_connection = (
+            HubConnectionBuilder()
+            .with_url(f"{self.host}/SensorHub?token={self.token}")
+            .configure_logging(logging.INFO)
+            .with_automatic_reconnect(
+                {
+                    "type": "raw",
+                    "keep_alive_interval": 10,
+                    "reconnect_interval": 5,
+                    "max_attempts": 999,
+                }
+            )
+            .build()
+        )
 
-        self._hub_connection.on("ReceiveSensorData", self.onSensorDataReceived)
+        self._hub_connection.on(
+            "ReceiveSensorData", self.on_sensor_data_received
+        )
         self._hub_connection.on_open(lambda: print("||| Connection opened."))
         self._hub_connection.on_close(lambda: print("||| Connection closed."))
-        self._hub_connection.on_error(lambda data: print(f"||| An exception was thrown closed: {data.error}"))
+        self._hub_connection.on_error(
+            lambda data: print(
+                f"||| An exception was thrown closed: {data.error}"
+            )
+        )
 
-    def onSensorDataReceived(self, data):
-        try:        
-            print(data[0]["date"]  + " --> " + data[0]["data"])
+    def on_sensor_data_received(self, data):
+        try:
+            print(data[0]["date"] + " --> " + data[0]["data"])
             date = data[0]["date"]
-            dp = float(data[0]["data"])
+            date_float = float(data[0]["data"])
 
-            self.analyzeDatapoint(date, dp)
-        except Exception as err:
+            self.analyze_datapoint(date, date_float)
+        except ConnectionError as err:
             print(err)
-    
-    def analyzeDatapoint(self, date, data):
-        if (data >= 80.0):                
-            self.sendActionToHvac(date, "TurnOnAc", 6)
-        elif (data <= 20.0):                
-            self.sendActionToHvac(date, "TurnOnHeater", 6)
 
-    def sendActionToHvac(self, date, action, nbTick):
-        r = requests.get(f"{self.HOST}/api/hvac/{self.TOKEN}/{action}/{nbTick}") 
-        details = json.loads(r.text)
-        print(details)
+    def analyze_datapoint(self, date, data):
+        if data >= 80.0:
+            self.send_action_to_hvac(date, "TurnOnAc", 6)
+        elif data <= 20.0:
+            self.send_action_to_hvac(date, "TurnOnHeater", 6)
 
-if __name__ == '__main__':
+    def send_action_to_hvac(self, date, action, nb_tick):
+        request = requests.get(
+            f"{self.host}/api/hvac/{self.token}/{action}/{nb_tick}", timeout=10
+        )
+        details = json.loads(request.text)
+        print(details + "\ndate : " + date)
+
+
+if __name__ == "__main__":
     main = Main()
     main.start()
-
-
-
--- test/test_main.py
+++ test/test_main.py
@@ -1,18 +1,15 @@
 import unittest
-import sys
-import requests
-import json
 import os
-from src.main import Main
-import mysql.connector as mysql
+import requests
 
 HOST = os.environ["HVAC_HOST"]
 
-class TestStringMethods(unittest.TestCase):
 
+class TestStringMethods(unittest.TestCase):
     def test_simulator_up(self):
-        r = requests.get(f"{HOST}/api/health") 
-        self.assertEqual("All system operational Commander !", r.text)
-    
-if __name__ == '__main__':
+        request = requests.get(f"{HOST}/api/health", timeout=10)
+        self.assertEqual("All system operational Commander !", request.text)
+
+
+if __name__ == "__main__":
     unittest.main()
