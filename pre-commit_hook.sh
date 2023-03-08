1915d340bbbee4150fc53d64a43de520b191d6e9
damiendesvent <damiendesvent@gmail.com>
2023-03-08 10:04:43 -0500

    v2 affichage clair de l'erreur du linter

--- pre-commit_hook.sh
+++ pre-commit_hook.sh
@@ -2,6 +2,7 @@
 
 echo "#!/bin/sh" > .git/hooks/pre-commit
 echo "black . --quiet" >> .git/hooks/pre-commit
+echo "echo $'Code impropre (score < 9). Plus d\'infos en affichant la sortie de commande !'" >> .git/hooks/pre-commit
 echo "pylint --fail-under=9 HVAC_LOG680-01_H2023_EQ12" >> .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
