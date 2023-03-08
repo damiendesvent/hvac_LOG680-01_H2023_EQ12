9ca049d20196111ffc3d6de69f9db4f52ff5cb16
damiendesvent <damiendesvent@gmail.com>
2023-03-08 10:36:38 -0500

    mise au propre code

--- src/main.py
+++ src/main.py
@@ -3,19 +3,12 @@ import sys
 import json
 import time
 import os
-import os
-import os
-import os
-import os
-import os
-import os
-import os
-import os
-import os
-import os
-import os
 import requests
 from signalrcore.hub_connection_builder import HubConnectionBuilder
+
+
+
+
 
 
 class Main:
