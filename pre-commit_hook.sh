ec6659f97365cfaa2b1ab0b9cb3ee3b14c88a7db
damiendesvent <damiendesvent@gmail.com>
2023-03-08 09:17:33 -0500

    modif fichier install pre-commit + test formatage main + modif message pre-commit

--- pre-commit_hook.sh
+++ pre-commit_hook.sh
@@ -2,6 +2,6 @@
 
 echo "#!/bin/sh" > .git/hooks/pre-commit
 echo "black ." >> .git/hooks/pre-commit
-echo "pylint HVAC_LOG680-01_H2023_EQ12" >> .git/hooks/pre-commit
+echo "pylint --fail-under=9 HVAC_LOG680-01_H2023_EQ12" >> .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
--- src/main.py
+++ src/main.py
@@ -3,6 +3,13 @@ import sys
 import json
 import time
 import os
+import os
+import os
+import os
+import os
+import os
+import os
+import os
 import requests
 from signalrcore.hub_connection_builder import HubConnectionBuilder
 
@@ -57,6 +64,13 @@ class Main:
                 f"||| An exception was thrown closed: {data.error}"
             )
         )
+
+
+
+
+
+
+
 
     def on_sensor_data_received(self, data):
         try:
