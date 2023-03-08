922bdbecd028b77a127ded9957283a99ce5c4055
damiendesvent <damiendesvent@gmail.com>
2023-03-08 09:50:59 -0500

    mise au propre output erreur git + test pre-commit

--- pre-commit_hook.sh
+++ pre-commit_hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "#!/bin/sh" > .git/hooks/pre-commit
-echo "black ." >> .git/hooks/pre-commit
-echo "pylint --fail-under=9 HVAC_LOG680-01_H2023_EQ12" >> .git/hooks/pre-commit
+echo "black . --quiet" >> .git/hooks/pre-commit
+echo "pylint --fail-under=9 HVAC_LOG680-01_H2023_EQ12 | tail -2" >> .git/hooks/pre-commit
 
 chmod +x .git/hooks/pre-commit
